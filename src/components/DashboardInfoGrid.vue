5c13af5ae937f65df1de6eb35de053afc2bf5849
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-03-24 14:05:17 -0700

    Update to home page

--- src/components/DashboardInfoGrid.vue
+++ src/components/DashboardInfoGrid.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="grid grid-rows-2 grid-cols-3 grid-flow-col gap-4">
-        <div class="flex items-center bg-white rounded-2xl">
-            <div class="w-full relative mx-2 py-8 pl-4 bg-brand-blue-6 rounded-2xl">
-                <div>
+        <div class="bg-white p-2 rounded-2xl">
+            <div class="w-full h-full relative flex items-center bg-brand-blue-6 rounded-2xl">
+                <div class="pl-4">
                     <div class="flex items-center space-x-2">
                         <p class="mb-1">Total Sol Staked</p>
                         <img src="../assets/info-icon.svg" alt="Info icon" />
@@ -17,9 +17,10 @@
                 </div>
             </div>
         </div>
-        <div class="flex items-center bg-white rounded-2xl">
-            <div class="w-full relative mx-2 py-8 pl-4 bg-brand-blue-6 rounded-2xl">
-                <div>
+
+        <div class="p-2 bg-white rounded-2xl">
+            <div class="w-full h-full relative flex items-center bg-brand-blue-6 rounded-2xl">
+                <div class="pl-4">
                     <div class="flex items-center space-x-2">
                         <p class="mb-1">Pools balance</p>
                         <img src="../assets/info-icon.svg" alt="Info icon" />
@@ -31,9 +32,10 @@
                 </div>
             </div>
         </div>
-        <div class="flex items-center bg-white rounded-2xl">
-            <div class="w-full relative mx-2 py-8 pl-4 bg-brand-blue-6 rounded-2xl">
-                <div>
+
+        <div class="p-2 bg-white rounded-2xl">
+            <div class="w-full h-full relative flex items-center bg-brand-blue-6 rounded-2xl">
+                <div class="pl-4">
                     <div class="flex items-center space-x-2">
                         <p class="mb-1">EPOC 265</p>
                         <img src="../assets/info-icon.svg" alt="Info icon" />
@@ -45,9 +47,9 @@
                 </div>
             </div>
         </div>
-        <div class="flex items-center bg-white rounded-2xl">
-            <div class="w-full relative mx-2 py-8 pl-4 bg-brand-blue-6 rounded-2xl">
-                <div>
+        <div class="p-2 bg-white rounded-2xl">
+            <div class="w-full h-full relative flex items-center bg-brand-blue-6 rounded-2xl">
+                <div class="pl-4">
                     <div class="flex items-center space-x-2">
                         <p class="mb-1">Farm balance</p>
                         <img src="../assets/info-icon.svg" alt="Info icon" />
@@ -59,23 +61,25 @@
                 </div>
             </div>
         </div>
-        <div class="row-span-2 bg-brand-blue-4 rounded-2xl">
-            <div class="relative m-3 py-8 px-4 bg-brand-blue-1 rounded-2xl text-white">
-                <p class="mb-1">Total Earnings</p>
-                <h3 class="font-bold text-2xl">
-                    $10,990.78
-                    <span class="text-sm font-semibold">+12.40%</span>
-                </h3>
-                <div class="mb-3">
-                    <img
-                        class="w-full"
-                        src="../assets/earnings-graph.png"
-                        alt="Earnings graph photo"
-                    />
+        <div class="row-span-2 p-2 bg-brand-blue-4 rounded-2xl">
+            <div class="relative h-full w-full bg-brand-blue-1 rounded-2xl text-white">
+                <div class="p-6">
+                    <p class="mb-1">Total Earnings</p>
+                    <h3 class="font-bold text-2xl">
+                        $10,990.78
+                        <span class="text-sm font-semibold">+12.40%</span>
+                    </h3>
+                    <div class="mb-3">
+                        <img
+                            class="w-full"
+                            src="../assets/earnings-graph.png"
+                            alt="Earnings graph photo"
+                        />
+                    </div>
+                    <button
+                        class="w-full px-3 py-4 text-brand-blue-1 bg-white rounded-full font-semibold"
+                    >Claim Earnings</button>
                 </div>
-                <button
-                    class="w-full px-3 py-4 text-brand-blue-1 bg-white rounded-full font-semibold"
-                >Claim Earnings</button>
             </div>
         </div>
     </div>
